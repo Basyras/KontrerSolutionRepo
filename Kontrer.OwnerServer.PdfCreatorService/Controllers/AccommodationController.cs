75f9912a0673e810ce9e07308eb8a8304a733275
Basyras <basyrascz@gmail.com>
2020-12-05 00:31:39 +0100

    Adding PdfCreator service

---
+++ Kontrer.OwnerServer.PdfCreatorService/Controllers/AccommodationController.cs
@@ -0,0 +1,26 @@
+﻿using Kontrer.Shared.Models.Pricing.Costs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kontrer.OwnerServer.PdfCreatorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccommodationController : ControllerBase
+    {
+        // GET: api/<AccommodationController>
+        [HttpGet]
+        public MemoryStream Get(AccommodationCost cost)
+        {
+            throw new NotImplementedException();
+        }      
+
+      
+    }
+}
