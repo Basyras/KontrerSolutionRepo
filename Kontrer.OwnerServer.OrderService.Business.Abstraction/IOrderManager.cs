3abc714c618a9834300da7ab1c5ec5e4dca2e2ea
Benek Martin <1martinbenek@seznam.cz>
2020-12-16 21:40:38 +0100

    Creating order Microservice

---
+++ Kontrer.OwnerServer.OrderService.Business.Abstraction/IOrderManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontrer.OwnerServer.OrderService.Business.Abstraction
+{
+    interface IOrderManager
+    {
+        void CreateOrder();
+        void RemoveOrder();
+        void EditOrder();
+        void GetOrders();
+
+        
+
+    }
+}
