41f701012cd6ed943b7c60fb7238f32e0ec396f7
Basyras <basyrascz@gmail.com>
2021-01-16 18:51:30 +0100

    adding tests

---
+++ OwnerServer/IdGeneratorService/Presentation/Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApiTests/IdGenerator/Data/InMemoryIdGeneratorStorage.cs
@@ -0,0 +1,38 @@
+﻿using Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApi.IdGenerator.Abstraction.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApiTests.IdGenerator.Data
+{
+    public class InMemoryIdGeneratorStorage : IIdGeneratorStorage
+    {
+
+        Dictionary<string, int> storage = new Dictionary<string, int>();
+        public Task<int> GetLastUsedId(string groupName)
+        {
+            TryCreate(groupName);
+            var id = storage[groupName];
+            return Task.FromResult(id);
+        }
+
+        public Task SetLastUsedId(string groupName, int lastUsedId)
+        {
+            TryCreate(groupName);
+            storage[groupName] = lastUsedId;
+            return Task.CompletedTask;
+        }
+
+
+        private void TryCreate(string groupName)
+        {
+            var groupExists = storage.TryGetValue(groupName, out _);
+            if (groupExists is false)
+            {
+                storage.Add(groupName, default);
+            }
+        }
+    }
+}
---
+++ OwnerServer/IdGeneratorService/Presentation/Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApiTests/IdGenerator/IdGeneratorManagerTests.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApi.IdGenerator;
+using Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApi.IdGenerator.Abstraction.Data;
+using Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApiTests.IdGenerator.Data;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kontrer.OwnerServer.IdGeneratorService.Presentation.AspApiTests.IdGenerator
+{
+    public class IdGeneratorManagerTests
+    {
+        [Fact]
+        public void ManagerReturnsUniqueIdBeyondCache()
+        {
+            var storageMoq = new InMemoryIdGeneratorStorage();
+            var manager = new IdGeneratorManager(storageMoq);
+
+            int currentId = 0;
+            int nextId = 0;
+
+            for (int i = 0; i < manager.CacheSize + 2; i++)
+            {
+                currentId = manager.CreateNewId(IdGeneratorManager.OrdersGroupName);
+                nextId = manager.CreateNewId(IdGeneratorManager.OrdersGroupName);
+                nextId.Should().Be(currentId + 1, "Generator managers must return only unique values");
+                currentId = nextId;
+            }
+
+
+        }
+
+        [Fact]
+        public void DifferentGroupsShouldHaveRedudantValues()
+        {
+            var storageMoq = new InMemoryIdGeneratorStorage();
+
+            var manager = new IdGeneratorManager(storageMoq);
+            var group1Id = manager.CreateNewId(IdGeneratorManager.OrdersGroupName);
+            var group2Id = manager.CreateNewId(IdGeneratorManager.CustomersGroupName);
+            group1Id.Should().Be(group2Id, "Two id groups should be independent of each other");
+        }
+
+        [Fact]
+        public void UnknownGroupNameShouldFail()
+        {
+            var storageMoq = new InMemoryIdGeneratorStorage();
+            var manager = new IdGeneratorManager(storageMoq);
+
+            Action action = () => { manager.CreateNewId("RandomGroupName"); };
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void KnownGroupNameShouldPass()
+        {
+            var storageMoq = new InMemoryIdGeneratorStorage();
+            var manager = new IdGeneratorManager(storageMoq);
+            _ = manager.CreateNewId(IdGeneratorManager.OrdersGroupName);
+
+        }
+    }
+}
