8cdd2b1bcdad6a4c66ca94ecdb865c5739e6b0d4
Basyras <basyrascz@gmail.com>
2021-02-22 21:59:48 +0100

    creating shared response base

---
+++ Kontrer.OwnerServer.Shared.MicroService.Abstraction/MessageBus/Requests/GenericResponseBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrer.OwnerServer.Shared.MicroService.Abstraction.MessageBus.Requests
+{
+    public abstract class GenericResponseBase<TResponse>
+    {
+        public TResponse Data { get; }
+
+        public GenericResponseBase(TResponse response)
+        {
+            Data = response;
+        }
+        
+    }
+}
