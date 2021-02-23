099e2199a06a7d7e6db1110e57194fba98111a72
Basyras <basyrascz@gmail.com>
2021-02-23 13:19:23 +0100

    removing comment

--- Kontrer.OwnerServer.Shared.Data.Abstraction/Repositories/IRepository.cs
+++ Kontrer.OwnerServer.Shared.Data.Abstraction/Repositories/IRepository.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 namespace Kontrer.OwnerServer.Shared.Data.Abstraction.Repositories
 {
     public interface IRepository 
-    {
-        //Should not implement save method when used with unit of work pattern
+    { 
     }
 }
